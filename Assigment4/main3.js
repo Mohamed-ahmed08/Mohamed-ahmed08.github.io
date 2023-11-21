34c6d92ff75b3e5e285863e4c222ef539cd617fa
Mohamed-ahmed08 <144464801+Mohamed-ahmed08@users.noreply.github.com>
2023-11-20 19:09:15 -0500

    Added an evil circle shape
    added a collision detect
    added a code that would enable the evil circle to move around the screen

--- Assigment4/main3.js
+++ Assigment4/main3.js
@@ -77,6 +77,45 @@ class Ball {
 
 }
 
+class EvilCircle extends Shape {
+    constructor(x, y) {
+        super(x, y, 20, 20);
+
+        this.color = 'white';
+        this.size = 10;
+        window.addEventListener("keydown", (e) => {
+            switch (e.key) {
+                case "a":
+                    this.x -= this.velX;
+                    break;
+                case "d":
+                    this.x += this.velX;
+                    break;
+                case "w":
+                    this.y -= this.velY;
+                    break;
+                case "s":
+                    this.y += this.velY;
+                    break;
+            }
+        });
+    }
+    collisionDetect() {
+        for (const ball of balls) {
+            if (ball.exists) {
+                const dx = this.x - ball.x;
+                const dy = this.y - ball.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + ball.size) {
+                    ball.exists = false;
+                    count--;
+                    para.textContent = 'Ball count: ' + count;
+                }
+            }
+        }
+    }
+}
 const balls = [];
 
 while (balls.length < 25) {
@@ -93,7 +132,10 @@ while (balls.length < 25) {
    );
 
   balls.push(ball);
+  count++;
+    para.textContent = 'Ball count: ' + count;
 }
+
 
 function loop() {
    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
